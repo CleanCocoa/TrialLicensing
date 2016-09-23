96a1be31ca3dc5b300c7723798a12510130795a1
Christian Tietze <christian.tietze@gmail.com>
2016-09-23 14:14:58 +0200

    change AppLicensing to a public static interface

--- TrialLicense/AppLicensing.swift
+++ TrialLicense/AppLicensing.swift
@@ -21,25 +21,119 @@ typealias LicenseChangeCallback = (_ licenseInformation:LicenseInformation) -> V
 /// The `delegate` will receive notifications about license changes proactively.
 public class AppLicensing {
 
+    public static private(set) var sharedInstance: AppLicensing?
+
+    public static var currentLicenseInformation: LicenseInformation? {
+        return sharedInstance?.currentLicenseInformation
+    }
+
+    /// Performs initial licensing setup:
+    /// 
+    /// 1. Sets up the `sharedInstance`,
+    /// 2. starts the trial timer,
+    /// 3. optionally reports back the current `LicenseInformation`.
+    ///
+    /// - parameter configuration: Settings used to verify licenses for 
+    ///   this app.
+    /// - parameter initialTrialDuration: Number of `Days` the time-based
+    ///   trial should run beginning at the first invocation. (Consecutive
+    ///   initialization calls will not alter the expiration date.)
+    /// - parameter delegate: Receiver of license state changes.
+    /// - parameter clock: Testing seam so you can see what happens if the 
+    ///   trial is up with manual or integration tests.
+    /// - parameter fireInitialState: Pass `true` to have the end of the 
+    ///   setup routine immediately call `delegate.licenseDidChange`.
+    ///
+    ///   Default is `false`.
+    ///
+    ///   The callback is dispatched asynchronously on the main queue.
+    public static func startUp(
+        configuration: LicenseConfiguration,
+        initialTrialDuration: Days,
+        delegate: AppLicensingDelegate,
+        clock: KnowsTimeAndDate = Clock(),
+        fireInitialState: Bool = false) {
+
+        guard !hasValue(AppLicensing.sharedInstance)
+            else { preconditionFailure("AppLicensing.startUp was called twice") }
+
+        AppLicensing.sharedInstance = {
+
+            let appLicensing = AppLicensing(configuration: configuration, initialTrialDuration: initialTrialDuration, delegate: delegate, clock: clock)
+
+            appLicensing.setupTrial(initialTrialDuration: initialTrialDuration)
+            appLicensing.configureTrialRunner()
+
+            return appLicensing
+        }()
+
+        if fireInitialState {
+            AppLicensing.reportCurrentLicenseInformation()
+        }
+    }
+
+    fileprivate static func reportCurrentLicenseInformation() {
+
+        guard let instance = AppLicensing.sharedInstance else { return }
+
+        instance.licenseDidChange(licenseInformation: instance.currentLicenseInformation)
+    }
+
+    public static func tearDown() {
+
+        guard let sharedInstance = AppLicensing.sharedInstance else { return }
+
+        sharedInstance.stopTrialRunner()
+
+        AppLicensing.sharedInstance = nil
+    }
+
+    fileprivate func setupTrial(initialTrialDuration: Days) {
+
+        guard !trialProvider.isConfigured else { return }
+
+        let trialPeriod = TrialPeriod(numberOfDays: initialTrialDuration,
+                                      clock: self.clock)
+        TrialWriter().store(trialPeriod: trialPeriod)
+    }
+
+    fileprivate func configureTrialRunner() {
+
+        self.trialRunner.startTrialTimer()
+    }
+
+    fileprivate func stopTrialRunner() {
+
+        self.trialRunner.stopTrialTimer()
+    }
+
+
+    // MARK: -
+
+    let clock: KnowsTimeAndDate
+    let trialProvider: TrialProvider
     let licenseInformationProvider: LicenseInformationProvider
     fileprivate(set) var register: RegisterApplication!
     fileprivate(set) var trialRunner: TrialRunner!
 
     public weak fileprivate(set) var delegate: AppLicensingDelegate?
 
-    /// - parameter configuration: Settings used to verify licenses for 
-    ///   this app.
-    /// - parameter delegate: Optionally, provide the `AppLicensingDelegate` 
-    ///   upon initialization already.
-    public init(configuration: LicenseConfiguration, delegate: AppLicensingDelegate? = nil) {
+    init(
+        configuration: LicenseConfiguration,
+        initialTrialDuration: Days,
+        delegate: AppLicensingDelegate? = nil,
+        clock: KnowsTimeAndDate = Clock()) {
+
+        self.clock = clock
 
         let licenseVerifier = LicenseVerifier(configuration: configuration)
         let licenseProvider = LicenseProvider()
-        let trialProvider = TrialProvider()
+        self.trialProvider = TrialProvider()
         self.licenseInformationProvider = LicenseInformationProvider(
             trialProvider: trialProvider,
             licenseProvider: licenseProvider,
-            licenseVerifier: licenseVerifier)
+            licenseVerifier: licenseVerifier,
+            clock: clock)
 
         self.trialRunner = TrialRunner(
             trialProvider: trialProvider,
@@ -63,7 +157,7 @@ public class AppLicensing {
 
     public var currentLicenseInformation: LicenseInformation {
 
-        return licenseInformationProvider.currentLicenseInformation
+        return self.licenseInformationProvider.currentLicenseInformation
     }
 
 
@@ -71,11 +165,17 @@ public class AppLicensing {
 
     fileprivate func licenseDidChange(licenseInformation: LicenseInformation) {
 
-        self.delegate?.licenseDidChange(licenseInformation: licenseInformation)
+        DispatchQueue.main.async {
+
+            self.delegate?.licenseDidChange(licenseInformation: licenseInformation)
+        }
     }
 
     fileprivate func didEnterInvalidLicense(name: String, licenseCode: String) {
 
-        self.delegate?.didEnterInvalidLicenseCode(name: name, licenseCode: licenseCode)
+        DispatchQueue.main.async {
+            
+            self.delegate?.didEnterInvalidLicenseCode(name: name, licenseCode: licenseCode)
+        }
     }
 }
